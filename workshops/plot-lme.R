4dcf7987045633554e853789a0a66c3e5edbfe55
martynmcfarquhar <martynmcfarquhar@googlemail.com>
2026-03-23 07:22:11 +0000

    Workshop updates

--- workshops/plot-lme.R
+++ workshops/plot-lme.R
@@ -5,13 +5,16 @@ plot.lme <- function(mod, vcov.id){
 
   resid.norm <- resid(mod, type="normalized")
   fit        <- fitted(mod)
-
   rand.effs  <- random.effects(mod)
-  levels     <- length(rand.effs)
+  levels     <- length(nlme::getGroupsFormula(mod,asList = TRUE))
   n.rand     <- 0
-  for (i in 1:levels){
-    rand.lev <- rand.effs[[i]]
-    n.rand   <- n.rand + dim(rand.lev)[2]
+  if (levels == 1L){
+    n.rand <- dim(rand.effs)[2]
+  } else {
+    for (i in 1:levels){
+      rand.lev <- rand.effs[[i]]
+      n.rand   <- n.rand + dim(rand.lev)[2]
+    }
   }
 
   n.rows <- 2 + ceiling(n.rand/2)
@@ -30,13 +33,22 @@ plot.lme <- function(mod, vcov.id){
   lines(lowess(fit,sqrt(abs(resid.norm))))
 
   ## Normal Q-Q of the random effects
-  for (i in 1:levels){
-    rand.lev   <- rand.effs[[i]]
-    rand.names <- names(rand.lev)
-    for (j in 1:dim(rand.lev)[2]){
-      RE <- rand.lev[,j]
-      qqnorm(RE, main=rand.names[j]) 
-      qqline(RE)
+  if (levels == 1L){ 
+    rand.names <- names(rand.effs)
+    for (i in 1:dim(rand.effs)[2]){
+        RE <- rand.effs[,i]
+        qqnorm(RE, main=rand.names[i]) 
+        qqline(RE)
+    }
+  } else {
+    for (i in 1:levels){
+      rand.lev   <- rand.effs[[i]]
+      rand.names <- names(rand.lev)
+      for (j in 1:dim(rand.lev)[2]){
+        RE <- rand.lev[,j]
+        qqnorm(RE, main=rand.names[j]) 
+        qqline(RE)
+      }
     }
   }
 
@@ -50,4 +62,4 @@ plot.lme <- function(mod, vcov.id){
   } else {
     warning('Marginal covariance structure not available for more than 2 levels')
   }
-}
+  }
